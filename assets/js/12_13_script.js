cec61c9eea06fb5fba19044e5504c3f16096a6cf
Yiijie <94723993+Yiijie@users.noreply.github.com>
2024-12-29 20:47:31 +0100

    Update 12_13_script.js

--- assets/js/12_13_script.js
+++ assets/js/12_13_script.js
@@ -9,10 +9,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Define colors for the districts with transparency (using rgba)
 const colors = [
     'rgba(0, 160, 0, 0.6)',    // #00A000 with 0.6 opacity
+    'rgba(0, 160, 0, 0.6)',    // #00A000
     'rgba(255, 255, 0, 0.6)',  // #FFFF00
+    'rgba(0, 160, 0, 0.6)',    // #00A000
+    'rgba(0, 160, 0, 0.6)',    // #00A000
+    'rgba(0, 160, 0, 0.6)',    // #00A000
+    'rgba(0, 160, 0, 0.6)',    // #00A000
+    'rgba(0, 160, 0, 0.6)',    // #00A000
+    'rgba(0, 100, 0, 0.6)',    // #006400
     'rgba(139, 0, 0, 0.6)',    // #8B0000
+    'rgba(255, 255, 0, 0.6)',  // #FFFF00
+    'rgba(0, 100, 0, 0.6)',    // #006400
+    'rgba(255, 255, 0, 0.6)',  // #FFFF00
+    'rgba(0, 100, 0, 0.6)',    // #006400
+    'rgba(0, 100, 0, 0.6)',    // #006400
+    'rgba(0, 100, 0, 0.6)',    // #006400
+    'rgba(139, 0, 0, 0.6)',    // #8B0000
+    'rgba(255, 0, 0, 0.6)',    // #FF0000
+    'rgba(255, 0, 0, 0.6)',    // #FF0000
     'rgba(255, 0, 0, 0.6)'     // #FF0000
 ];
+
 
 // Function to get color based on district number
 function getDistrictColor(code) {
